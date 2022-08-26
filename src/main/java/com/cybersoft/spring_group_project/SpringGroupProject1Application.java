19ba827270aa23f1b68a7f032404013c2dfabd3b
trivi <minhtri.cm2001@gmail.com>
2022-08-27 06:31:50 +0700

    create project

---
+++ src/main/java/com/cybersoft/spring_group_project/SpringGroupProject1Application.java
@@ -0,0 +1,13 @@
+package com.cybersoft.spring_group_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGroupProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGroupProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybersoft/spring_group_project/SpringGroupProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersoft.spring_group_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGroupProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
